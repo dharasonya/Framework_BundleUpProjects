3fa0419fee897bcf502c775eef918e3db4c34864
Sonya <sonya@gmail.com>
2025-01-31 21:21:04 +0530

    added new

--- src/test/java/BundleUp/Bundle_Automation_Projects/TestTask.java
+++ src/test/java/BundleUp/Bundle_Automation_Projects/TestTask.java
@@ -34,7 +34,7 @@ public class TestTask {
 		driver=new FirefoxDriver();
 		
 		driver.get("https://epaysit.eftapme.com/FEESCRM/");
-		System.out.println("hi");
+		System.out.println("hi-1");
 		
 		
 
